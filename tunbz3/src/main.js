fabe12655dc7eec508429c2053aaf60d68d80216
Deoncn <2210633536@qq.com>
2023-08-19 11:53:51 +0800

    豊日

--- tunbz3/src/main.js
+++ tunbz3/src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import router from './router/index.js'
 import './assets/main.css'
 
+// 前端组件库 {Naive UI}
+import naive from 'naive-ui'
+
 
 const app = createApp(App)
 
 
 app.use(router)
+app.use(naive)
 app.mount('#app')
 
 
